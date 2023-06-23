ef434f8710fc8f53a7a22ea73f6c7069d7b2dc31
dilichev <58857267+ILICHEV-D@users.noreply.github.com>
2023-06-23 14:05:08 +0300

    Squashed commit of the following:
    commit cea182b9692fe9bae80aac713f75362197bbd8b2
    Merge: 0398318 5260940
    Author: ILICHEV-D <danil.ilich@icloud.com>
    Date:   Mon Jun 19 20:39:33 2023 +0300
    
        Merge pull request #1 from ILICHEV-D/hw-1
    
        Homework 1
    
    commit 5260940155e8b4a4b333e0ddbd1b0f53523ab560
    Author: dilichev <58857267+ILICHEV-D@users.noreply.github.com>
    Date:   Mon Jun 19 20:38:57 2023 +0300
    
        fix2
    
    commit 0769b5bd8da9f1bbbb65ce119b59f0b083614219
    Author: dilichev <58857267+ILICHEV-D@users.noreply.github.com>
    Date:   Mon Jun 19 20:35:00 2023 +0300
    
        fix

--- YaSchool/Services/FileCache/FileCache.swift
+++ YaSchool/Services/FileCache/FileCache.swift
@@ -20,7 +20,7 @@ import Foundation
  - Provide a mechanism to protect against duplication of tasks (by comparing IDs)
  */
 
-class FileCache: FileCacheType {
+final class FileCache: FileCacheType {
     
     private(set) var todoItems: [TodoItem]
     private let filename: String
